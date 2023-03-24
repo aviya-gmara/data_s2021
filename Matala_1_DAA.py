e65fb55a8ce2ded333d787c8648eb773d08a76ce
aviya-gmara <82819879+aviya-gmara@users.noreply.github.com>
2023-03-24 11:34:55 +0300

    Update Matala_1_DAA.py

--- Matala_1_DAA.py
+++ Matala_1_DAA.py
@@ -12,7 +12,7 @@ X3= 9.8
 
 
 def my_func(x1,x2,x3):
-    #בדיקה האם כלל המשתנים הם מסוג Float. במידה והתנאי מתקיים מתבצע החישוב
+    #Float. בדיקה האם כלל המשתנים הם מסוג -במידה והתנאי מתקיים מתבצע החישוב
     if isinstance(x1,float) and isinstance(x2, float) and isinstance(x3,float) == True:
         mone= (x1+x2+x3)*(x2+x3)*x3
         mechne= (x1+x2+x3)
@@ -21,7 +21,8 @@ def my_func(x1,x2,x3):
             return "Not a number – denominator equals zero"
  #החזרת התשובה
         return mone/mechne
-במידה והתנאי הראשון לא התקיים--> יש לחלק בין שני סגגי שגיאות:
+במידה והתנאי הראשון לא התקיים--> יש לחלק בין שני סוגי שגיאות:
+
     #1 אם int--> מחזיר שגיאה שנדרש flout
     elif isinstance(x1,int) or isinstance(x2, int) or isinstance(x3,int) == True:
             return "Error: parameters should be float"
@@ -37,7 +38,7 @@ print(answer)
 ##---------------------------------
 
 
-    # הפונקציה מקבלת מחרוזת אחת (מילה מ text) ומחזירה את המחרוזת
+    #  (מילה מ text) הפונקציה מקבלת מחרוזת אחתומחזירה את המחרוזת
     #כשהתווים שלה בסדר הפוך [::-1] ובאותיות קטנות .lower
 
 def revword(word:str) -> str:
@@ -45,24 +46,24 @@ def revword(word:str) -> str:
 
 
 def countword() -> int:
-    # הפונקציה פותחת את הקובץ text.txt וקוראת את המילה הראשונה בקובץ.
+    #  text.txt הפונקציה פותחת את הקובץ וקוראת את המילה הראשונה בקובץ.
     #.strip() יסייע לנו בלהוריד את הרווחים שקיימים בשורה
     word = ""
     with open("text.txt", "r") as f:
         word = f.readline().strip()
         count = 0
-        # הפונקציה עוברת על כל השורות של הקובץ text.txt, ומחזירה את מספר "המופעים" של המילה המופיעה בשורה הראשונה בקובץ text, מתחילתו ועד סופו.
+        # text.txt, הפונקציה עוברת על כל השורות של הקובץ ומחזירה את מספר "המופעים" של המילה המופיעה בשורה הראשונה בקובץ מתחילתו ועד סופו.
         for line in f:
             words_line = line.strip().split()
             for w in words_line:
                 if revword(w) == word:
                     count += 1
-                    #הוספה של 1 ל count  בכדי להגיע למספר המופעים *הכולל* בטקסט של המילה הראשונה
+                    # ל count  בכדי להגיע למספר המופעים *הכולל* בטקסט של המילה הראשונה -->הוספה של 1
                     #ולא רק של שאר המופעים בשאר השורות
         return count + 1
     
 
-# קריאה לפונקציה revword עם הפרמטר "OLLEH" במידה ורוצים לעשות test לפוקנציה המרה בלבד
+# "OLLEH" עם הפרמטר  test במידה ורוצים לעשות  קריאה לפונקצית המרה בלבד
 #print(revword("OLLEH"))
 
 # קריאה לפונקציה countword
